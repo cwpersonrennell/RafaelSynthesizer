a44a2ed587cf2ca177b5ff5fb9cb3d81eb24b8e0
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-06-13 10:28:18 -0700

    Update binarychoice.py

--- binarychoice.py
+++ binarychoice.py
@@ -8,10 +8,8 @@ Created on Wed May 31 08:28:14 2023
 import pygame
 from pygame import joystick
 pygame.init()
-joystick.init()
-joysticks = [joystick.Joystick(x) for x in range(joystick.get_count())]
-for joy in joysticks:
-    joy.init()
+
+joysticks = {}
     
 choices = [["yes","no"]]
 
@@ -20,23 +18,73 @@ for bchoice in choices:
     filenames.append(["./mp3/"+bchoice[0]+".mp3","./mp3/"+bchoice[1]+".mp3"])
     
 pygame.mixer.music.load(filenames[0][0])
+DEBUG = False
+
+def dprint(string):
+    if DEBUG:
+        print(string)
+
+JOYSTICK = False
+YESBUTTON =-1
+NOBUTTON = -1
 def joyStickHandler(event):
+    global JOYSTICK
+    global YESBUTTON
+    global NOBUTTON
+    if event.type == pygame.JOYDEVICEADDED:
+        joy = joystick.Joystick(event.device_index)
+        joysticks[joy.get_instance_id()]=joy
+
+        dprint(f"Joystick {joy.get_instance_id()} connected")
+        dprint(f"  GUID: {joy.get_guid()}")
+        dprint(f"  Name: {joy.get_name()}")
+
+    if event.type == pygame.JOYDEVICEREMOVED:
+        if event.instance_id == JOYSTICK.get_instance_id():
+            JOYSTICK = False
+            YESBUTTON = -1
+            NOBUTTON = -1
+        del joysticks[event.instance_id]
+
+        dprint(f"Joystick {event.instance_id} removed")
+        
     if event.type == pygame.JOYBUTTONDOWN:
-        if(event.joy == 1):return
-        if(event.button == 10):
+        if not JOYSTICK:
+            dprint("Assigning Joystick...")
+            JOYSTICK = joysticks[event.instance_id]
+        dprint(f"Joy: {event.instance_id}")
+
+        dprint(JOYSTICK.get_instance_id())
+
+        if(event.instance_id != JOYSTICK.get_instance_id()):return
+        
+        if YESBUTTON == -1:
+            YESBUTTON = event.button
+        elif YESBUTTON!=event.button and NOBUTTON == - 1:
+            NOBUTTON = event.button
+        
+        if(event.button == YESBUTTON):
             pygame.mixer.music.load(filenames[0][0])
-        if(event.button == 11):
+        if(event.button == NOBUTTON):
             pygame.mixer.music.load(filenames[0][1])
+    
     if event.type == pygame.JOYBUTTONUP:
-        if(event.joy == 1):return
+        if not JOYSTICK:
+            dprint("Assigning Joystick...")
+            JOYSTICK = joysticks[event.instance_id]
+        if(event.instance_id != JOYSTICK.get_instance_id()):return
         pygame.mixer.music.play()
                 
-try:    
-    while(len(joysticks)>1):
+try:
+    while(1):    
         for e in pygame.event.get():
             joyStickHandler(e)
 except KeyboardInterrupt:
     pygame.mixer.music.unload()
     pygame.quit()
     print("Goodbye!")
+
+except:
+    pygame.mixer.music.unload()
+    pygame.quit()
     
